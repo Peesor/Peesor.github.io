b96d3228d8a39470b3597864584d239dfd1e5350
Peesor <112574418+Peesor@users.noreply.github.com>
2022-11-22 18:43:43 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charest="utf-8">
+<meta lang="pl">
+<title>szkoła</title>
+</head>
+
+
+<h1 align="center">witam </h1>
+
+<body style="background-color: teal"align="center">
+
+
+<form action="zaloguj.php" method="post">
+
+Login:<br/> <input type="text" name="login" /> <br/>
+Haslo:<br/> <input type="password" name="haslo" /> <br/>
+<input type="submit" value="zaloguj się"/>
+
+</FORM>
+
+</body>
+
+</html>
