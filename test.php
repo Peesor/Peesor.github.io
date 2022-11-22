f9e587b7454497930f52ad5d80d1e041ce0c7b6c
Peesor <112574418+Peesor@users.noreply.github.com>
2022-11-22 19:01:42 +0100

    test11

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	
+	if ((!isset($_POST['login'])) || (!isset($_POST['haslo'])))
+	{
+		header('Location: index.php');
+		exit();
+	}
+
+	
+	$login = $_POST['login'];
+    $haslo = $_POST['haslo'];
+
+	
+
+
+echo $login,"  ",$haslo;
+
+
+
+
+
+
+
+
+
+
+
