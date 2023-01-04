73c3b6bcb3c5ede7a4988b570474c094278cae10
Peesor <112574418+Peesor@users.noreply.github.com>
2023-01-04 07:49:38 +0100

    new peesor

--- Nowy folder/timer.js
+++ Nowy folder/timer.js
@@ -13,3 +13,16 @@ function odliczanie()
 		 
 		 setTimeout("odliczanie()",10000);
 	}
+function TestCOMPort()
+{
+  var ForWriting = 2;
+  var TriStateFalse = 0;
+
+  var fso = new ActiveXObject("Scripting.FileSystemObject");
+  var f = fso.OpenTextFile("COM1:", ForWriting, false, TriStateFalse);
+  // Write data to the port
+  f.Write("A");
+  f.Write(" ");
+  f.Write("\x1B");
+  f.Close();
+}
